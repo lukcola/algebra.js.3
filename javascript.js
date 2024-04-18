00594d66c8aba184a51737943970de22a82d939d
LP <lukcola10@gmail.com>
2024-04-18 18:07:16 +0200

    drugi push

---
+++ javascript.js
@@ -0,0 +1,19 @@
+var myString = "Ovo je moj \"string\".";
+console.log(myString);
+
+var duljina = myString.length;
+console.log(`Duljina teksta je: ` + duljina + ` znakova.`);
+
+console.log(`Znak na mjestu 12 je: ` + myString.charAt(12) + `.`);
+
+console.log(myString.trim()); // trim() removes whitespace from both sides of a string.
+
+console.log(myString.trimEnd()); //.trimStart()
+
+console.log(myString.indexOf(`j`));
+
+console.log(myString.replace(`moj`, `naš`)); //.replaceAll()
+
+console.log(myString.toLocaleUpperCase());
+
+console.log(myString.substring(7, myString.length));
